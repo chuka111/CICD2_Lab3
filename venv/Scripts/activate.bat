36ace1630f27f93d749ff7b4cb77669a62acfeb0
chuka <g00414637@atu.ie>
2025-10-03 10:22:08 +0100

    added required files and used pip freeze

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\G00414637@atu.ie\OneDrive - Atlantic TU\CICD 2\CICD2_lab1\venv
+set VIRTUAL_ENV=C:\Users\G00414637@atu.ie\OneDrive - Atlantic TU\CICD 2\Lab 2\lab2week2\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
